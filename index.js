a93016f745493ed2f5099c526505cf557ff68850
Tincho develops <martinlabastie.p@gmail.com>
2022-09-09 15:40:34 -0300

    old syntax + users commented

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import Express from "express";
-import Mongoose from "mongoose";
-import Workers from "./workerSchema.js";
-import Users from "./UserSchema.js";
-import Cors from "cors";
+const express = require ("express");
+const mongoose =  require ("mongoose") ;
+const workers =  require ("./workerSchema.js") ;
+const users =  require ("./UserSchema.js") ;
+const cors =  require ("cors") ;
 
 // // App config
 
@@ -15,11 +15,11 @@ const port = process.env.PORT || 8080;
     
 
 // Middlewares
-    app.use(Express.json());
-    app.use(Cors());
+    app.use(express.json());
+    app.use(cors());
 
 //DB config
- Mongoose.connect(connectionUrl, {
+ mongoose.connect(connectionUrl, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     useUnifiedTopology: true,
@@ -33,49 +33,49 @@ app.get("/", (req, res) => {
 app.post("/workers", (req, res) => {
     const workerCard = req.body;
 
-    Workers.create(workerCard, (err,data)=>{
+    workers.create(workerCard, (err,data)=>{
         if(err) res.status(500).send(err) 
         res.status(201).send(data)
     })
 })
-app.post("/users", (req, res) => {
-    const userCard = req.body;
+// app.post("/users", (req, res) => {
+//     const userCard = req.body;
 
-    Users.create(userCard, (err,data)=>{
-        if(err) res.status(500).send(err);
-        else res.status(201).send(data);
-    })
-})
+//     users.create(userCard, (err,data)=>{
+//         if(err) res.status(500).send(err);
+//         else res.status(201).send(data);
+//     })
+// })
 
 app.get("/workers", (req, res) => {
-    Workers.find( (err,data) => {
+    workers.find( (err,data) => {
         if(err) res.status(500).send(err);
         res.status(200).send(data);
     })
 })
-app.get("/users", (req, res) => {
-    Users.find( (err,data) => {
-        if(err)  res.status(500).send(err)
-        res.status(200).send(data)
-    })
-})
+// app.get("/users", (req, res) => {
+//     users.find( (err,data) => {
+//         if(err)  res.status(500).send(err)
+//         res.status(200).send(data)
+//     })
+// })
 
-app.delete("/users/:userId", (req,res) =>{
-    let userId = req.params.userId;
+// app.delete("/users/:userId", (req,res) =>{
+//     let userId = req.params.userId;
 
-    Users.findById(userId, (err, user) =>{
-    if(err) res.status(500).send({message: `Error finding ID user: ${err}`});
+//     users.findById(userId, (err, user) =>{
+//     if(err) res.status(500).send({message: `Error finding ID user: ${err}`});
     
-        user.remove(err => {
-            if(err) res.status(500).send({message: `Error REMOVING user: ${err}`});
-            res.status(200).send({message: "User SUCCESFULLY deleted"})
-        })
-    })
-})
+//         user.remove(err => {
+//             if(err) res.status(500).send({message: `Error REMOVING user: ${err}`});
+//             res.status(200).send({message: "User SUCCESFULLY deleted"})
+//         })
+//     })
+// })
 app.delete("/workers/:workerId", (req,res) =>{
     const workerId = req.params.workerId;
 
-    Workers.findById(workerId, (err, worker) =>{
+    workers.findById(workerId, (err, worker) =>{
         if(err) res.status(500).send({message: `Error finding worker id: ${err}`});
             worker.remove(err => {
                 if(err) res.status(500).send({message: `Error REMOVING worker: ${err}`});
@@ -87,21 +87,21 @@ app.put("/workers/:workerId", (req, res) =>{
     let workerId = req.params.workerId;
     let updateBody = req.body;
 
-    Workers.findByIdAndUpdate(workerId, updateBody, (err, workerUpdated) => {
+    workers.findByIdAndUpdate(workerId, updateBody, (err, workerUpdated) => {
         if(err) res.status(500).send({message: `Error finding -EEESEESYO- ID to update: ${err}`});
         res.status(200).send({updatedData: updateBody})
     })
 })
-app.put("/users/:userId", (req,res) => {
-    let userId = req.params.userId;
-    let updateBody = req.body;
+// app.put("/users/:userId", (req,res) => {
+//     let userId = req.params.userId;
+//     let updateBody = req.body;
 
-    Users.findByIdAndUpdate(userId, updateBody, (err,userUpdated) => {
-        if(err) res.status(500).send({message: `error on Finding id to UPDATE: ${err}`}); 
-        res.status(200).send({newUserData: userUpdated})
-    })
+//     users.findByIdAndUpdate(userId, updateBody, (err,userUpdated) => {
+//         if(err) res.status(500).send({message: `error on Finding id to UPDATE: ${err}`}); 
+//         res.status(200).send({newUserData: userUpdated})
+//     })
 
-})
+// })
 
 // Listener
 app.listen(port, ()=> {
